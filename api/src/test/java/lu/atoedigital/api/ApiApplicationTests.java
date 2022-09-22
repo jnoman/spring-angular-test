4263f73bfe8c172dd8a4c9b4c9fcb82834f68bed
jnoman <jamalnoman62@gmail.com>
2022-09-22 17:16:01 +0100

    Initial commit

---
+++ api/src/test/java/lu/atoedigital/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.atoedigital.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
